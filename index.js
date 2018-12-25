3d33d185c16dae3c3d6a40b9473ca8559c78a607
akshay931 <akshayb3129@gmail.com>
2018-12-25 14:35:11 +0530

    a

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var http = require('http');
-
+var fs = require('fs');
 http.createServer(function(req,res){
-
+fs.appendFile('new.txt',res.url+"\n",function(err){
+if(err) console.log("error occured");
+console.log('request from '+res.url);
+)}
 res.writeHead(200,{'Content-Type':'text/plain'});
 res.end("my node app");
 
