f50c16c885ad6309eabf26f2933d0eb2dbaefbd0
AKSHAY <akshayb3129@gmail.com>
2018-12-25 13:54:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var port=process.env.PORT || 3000
+const port=process.env.PORT || 3000
  http.createServer((req, res) => {
 res.setHeader('Content-Type', 'text/html');
 
