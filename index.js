8ed633d1ca39b4b0a365b076ad78a4408d829819
AKSHAY <akshayb3129@gmail.com>
2018-12-25 13:50:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function(req,res){
-	fs.readFile('index.html',function(err,data){
-		res.writeHead(200.{'Content-Type':'text/html'});
-		res.write(data);
-		res.end();
-	});
-}).listen(8080);
-console.log("server running on 127.0.0.1:8080");
+const http = require('http');
 
+const port=process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+
+res.statusCode = 200;
+
+res.setHeader('Content-Type', 'text/html');
+
+res.end('<h1>Hello World</h1>');
+
+});
+
+server.listen(port,() => {
+
+console.log(`Server running at port `+port);
+
+});
