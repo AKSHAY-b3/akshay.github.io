a6066c01ea47cdd81814acd54914f070619c2501
AKSHAY <akshayb3129@gmail.com>
2018-12-25 13:29:50 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,10 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>My Javascript</title>
-	
-</head>
-<body>
-<h1>Hello Friends This is Akshay
-</body>
-</html>
+var http = require('http');
+var fs = require('fs');
+http.createServer(function(req,res){
+	fs.readFile('index.html',function(err,data){
+		res.writeHead(200.{'Content-Type':'text/html'});
+		res.write(data);
+		res.end();
+	});
+}).listen(8080);
+console.log("server running on 127.0.0.1:8080");
+
