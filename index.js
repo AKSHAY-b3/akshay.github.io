a532789b949fd9be28b187ec3f4a3843964b6b4c
akshay931 <akshayb3129@gmail.com>
2018-12-25 14:28:50 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
- http.createServer((req, res) => {
-res.setHeader('Content-Type', 'text/html');
+http.createServer(function(req,res){
 
-res.end('<h1>Hello World</h1>');
+res.writeHead(200,{'Content-Type':'text/plain'});
+res.end("my node app");
 
 }).listen(process.env.PORT,console.log("server running"));
