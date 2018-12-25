44c3a3dfab809474156efcd5b38650b28e32026b
akshay931 <akshayb3129@gmail.com>
2018-12-25 14:42:42 +0530

    commited

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function(req,res){
-fs.appendFile('new.txt',req.url+"\n",function(err){
-if(err) console.log("error occured");
-console.log('request from '+req.url);
-})
 res.writeHead(200,{'Content-Type':'text/plain'});
+console.log(req.url);
 res.end("my node app");
-
 }).listen(process.env.PORT,console.log("server running"));
